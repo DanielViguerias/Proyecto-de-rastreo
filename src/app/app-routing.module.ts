c5274bdb0130c7c0aaad4eb14a87bd6f847086aa
Axel2219 <80271196+Axel2219@users.noreply.github.com>
2021-06-30 12:43:51 -0500

    Add files via upload
    crud usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { CrearusuarioComponent } from './components/dashboard/crearusuario/crearusuario.component';
+import { CrearusuarioComponent } from './components/dashboard/usuario/crearusuario/crearusuario.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -7,11 +7,13 @@ import { LugaresComponent } from './components/dashboard/lugares/lugares.compone
 import { MapaComponent } from './components/dashboard/mapa/mapa.component';
 import { RecursosComponent } from './components/dashboard/recursos/recursos.component';
 import { ReportesComponent } from './components/dashboard/reportes/reportes.component';
-import { UsuariosComponent } from './components/dashboard/usuarios/usuarios.component';
+import { UsuariosComponent} from '../app/components/dashboard/usuario/usuarios.component';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/inicio/login/login.component';
 import { AccessguardGuard } from './guards/accessguard.guard';
+import { EditarusuarioComponent } from './components/dashboard/usuario/editarusuario/editarusuario.component';
+import { BorrarusuarioComponent } from './components/dashboard/usuario/borrarusuario/borrarusuario.component';
 
 const routes: Routes = [
   {path:"", redirectTo:'/inicio', pathMatch:'full'},
@@ -22,12 +24,14 @@ const routes: Routes = [
   {path: 'dashboard', canActivate:[AccessguardGuard],component: DashboardComponent, children:[
     {path:'home', component:MapaComponent},
     {path:'usuario', component:UsuariosComponent,children:[
-      {path:'crearusuario',component:CrearusuarioComponent}
+      {path:'crearusuario',component:CrearusuarioComponent},
+      {path:'editarusuario/:id',component:EditarusuarioComponent},
+      {path:'borrarusuario/:id',component:BorrarusuarioComponent},
+      
     ]},
     {path:'lugar', component:LugaresComponent},
     {path:'recurso', component:RecursosComponent},
-    {path:'reporte', component:ReportesComponent},
-    
+    {path:'reporte', component:ReportesComponent}
   ]}
   ,
   {path:'**', redirectTo:'/inicio',pathMatch:'full'}
