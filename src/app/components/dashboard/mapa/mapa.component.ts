831b6ea63dba7fe14e501444a277dd4f8df9d91a
ermak <ermakmx@gmail.com>
2021-07-18 23:56:12 -0500

    se agrega infowindow

--- src/app/components/dashboard/mapa/mapa.component.ts
+++ src/app/components/dashboard/mapa/mapa.component.ts
@@ -1,274 +1,226 @@
-import { PortalInjector } from '@angular/cdk/portal';
-import { AfterViewInit, Component } from '@angular/core';
-import MarkerClusterer from '@googlemaps/markerclustererplus';
-import { } from 'google.maps';
-import { map } from 'rxjs/operators';
-import { tokenGetter } from '../../../app.module';
-import { stringify } from '@angular/compiler/src/util';
-import { GuardsCheckStart } from '@angular/router';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {} from 'google.maps';
 import jwtDecode from 'jwt-decode';
-
-interface Lugar{
-
-  nombre:string,
-  geocerca: google.maps.Polygon
-}
-
-interface user{
-  aud: string,​
-  exp: number,
-  idUsuario: number,
-  ​iss: string,​
-  role: string,​
-  sub: string
-  }
-@Component({
-  selector: 'app-mapa',
-  templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.css']
-})
+import {Lugar, sitio, user} from "./mapa.interfaces"
+import {MapaService} from "../../../services/mapa.service"
+import {urlApi} from 'src/environments/environment';
+import {Observable} from 'rxjs';
 
 
-
-export class MapaComponent implements AfterViewInit {
-
+@Component({selector: 'app-mapa', templateUrl: './mapa.component.html', styleUrls: ['./mapa.component.css']})
 
 
- 
-  constructor() { }
+// --- Inicializacion despues de que se inicia la vista --- //
+export class MapaComponent implements OnInit {
+    lugares : Array < sitio >= []
 
-  ngAfterViewInit(): void {
 
-    let map: google.maps.Map;
-    let marcadores: google.maps.Marker[] = [];
-    let geocercas: Lugar[]=[];
-    
-  
-    
-    
+    constructor(private map2service : MapaService) {}
 
-    // for(let i=0;cargageo.lengh<i;i++){
-    //   for(let j=0;cargageo[i].length;j++){
-    //     console.log(cargageo[0])    
-    //   }
-    // }
+    ngOnInit(): void {
 
-    //console.log(cargageo)
 
-    
-    
-    
-    const center: google.maps.LatLngLiteral = { lat: 21.921427882183703 , lng: -102.29496958768634 };
-    function initMap(): void {
-      map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center,
-        zoom: 18,
-        mapTypeControl: false,
-        streetViewControl: false,
-        fullscreenControl: false,
-        mapTypeId: "terrain"
-      });
-      let poligono = new google.maps.Polygon({
-        fillColor: "#f43f01",
-          fillOpacity: 0.5,
-          strokeWeight: 1,
-          clickable: true,
-          editable: true,
-          zIndex: 1,
-          draggable: false,
-          paths:geocercas? geocercas:[]
-      });
-     // let cargageo = cargargeocercas();
+        this.map2service.getLugares().subscribe(data => {
+            this.lugares = data;
+            console.log(data);
 
-     
 
-      
-      
-      //const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        })
 
-    /*   var geocerca = new google.maps.Polygon({
-        paths: [],
-        editable: true
-      }); */
-      
-      /* const markers = locations.map((location, i) => {
-        return new google.maps.Marker({
-          position: location,
-          label: labels[i % labels.length],
-        });
-      }); */
-    /*  new MarkerClusterer(map, markers, {
-        imagePath:
-          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-      });     */
-      //-- poligonos--//
-      
-      //  const paths = [
-      //   { lat:  29.82427726324196 , lng: -110.2277761270849 },
-      //   { lat: 29.92437726324196, lng: -110.3287761270849 },
-      //   { lat: 29.72447726324196, lng: -110.4297761270849 },]
-     /* const flightpath = new google.maps.Polygon({
-        paths: mytrip,
-        strokeColor:"#0000FF",
-        strokeOpacity:0.8,
-        strokeWeight:2,
-        fillColor:"#0000FF",
-        fillOpacity:0.8
-      })
-      flightpath.addListener('mouseover',()=>{
-        console.log("paso por la figura");
-      });
-      flightpath.setMap(map); */
-      
-      // --- asignar variables de dibujo --- 
 
-      const drawTools = new google.maps.drawing.DrawingManager({
-        drawingMode: null,
-        drawingControl: true,
-        drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: [
-            google.maps.drawing.OverlayType.MARKER,
-            google.maps.drawing.OverlayType.POLYGON
-          ],
-        },
-        
-        markerOptions: {
-          icon: "../../../../assets/img/building.png",
-          draggable: true,
-          clickable:true
+        let map: google.maps.Map;
+        let marcadores: google.maps.Marker[] = [];
+        let geocercas: Lugar[] = [];
+        let index: number = 0;
+        let sitios: Observable<sitio[]>;
 
-        },
-        polygonOptions: {
-          fillColor: "#f43f01",
-          fillOpacity: 0.5,
-          strokeWeight: 1,
-          clickable: true,
-          editable: false,
-          zIndex: 1,
-          draggable: false
-          
-
-        },
-      });
-
-      /* google.maps.event.addListener(drawTools, 'polygoncomplete', function(polygon) {
-        console.log(polygon.type)
-          
-        });  */
-        if(IsAdmin()){
-          drawTools.setMap(map);
+        var addListenersOnPolygon = function (polygon) {
+            google.maps.event.addListener(polygon, 'click', function (event) {});
         }
-       
+        // --- Centrar en Arkus Nexus Aguascalientes --- //
+        const center: google.maps.LatLngLiteral = {
+            lat: 21.921427882183703,
+            lng: -102.29496958768634
+        };
+        const initMap = () : void => { // --- inicializar el mapa --- //
+            map = new google.maps.Map(document.getElementById("map")as HTMLElement, {
+                center,
+                zoom: 18,
+                mapTypeControl: false,
+                streetViewControl: false,
+                fullscreenControl: false,
+                mapTypeId: "terrain"
+            });
+
+            // --- asignar variables de dibujo ---
+
+            const drawTools = new google.maps.drawing.DrawingManager({
+                drawingMode: null,
+                drawingControl: true,
+                drawingControlOptions: {
+                    position: google.maps.ControlPosition.TOP_CENTER,
+                    drawingModes: [google.maps.drawing.OverlayType.MARKER, google.maps.drawing.OverlayType.POLYGON]
+                },
+
+                markerOptions: {
+                    icon: "../../../../assets/img/building.png",
+                    draggable: true,
+                    clickable: true
+
+                },
+                polygonOptions: {
+                    fillColor: "#0008ff",
+                    fillOpacity: 0.5,
+                    strokeWeight: 1,
+                    clickable: true,
+                    editable: false,
+                    zIndex: 1,
+                    draggable: false
 
 
-       if(cargargeocercas()){
-        let cargageo = cargargeocercas();
-         cargageo.forEach(element => {
-         
-          console.log(element);
-          
-          var pats = element.geocerca
-          
-          let poli = new google.maps.Polygon({
-             fillColor: "#f43f01",
-               fillOpacity: 0.5,
-               strokeWeight: 1,
-               clickable: true,
-               editable: false,
-               zIndex: 1,
-               draggable: false,
-               paths: pats
-           });
-           
-           geocercas.push({'nombre':'nombre','geocerca':pats})
-           //console.log(element);
-          // console.log(poli);
-           poli.setMap(map);
-     
-     
-         });
-       }
-       
-       google.maps.event.addListener(drawTools, "polygoncomplete", function(polygon) {
-        
-       // console.log(polygon.latLngs.we[0].we);
-        var paths = polygon.latLngs.we[0].we;
-        
-        geocercas.push({
-       'nombre':'nuevo',
-       'geocerca':paths})
-       //console.log(geocercas)
-          console.log(geocercas)
-       guardageocercas(JSON.stringify(geocercas));
-        
-        /* 
-            poligono.setPaths = event.latLngs.we[0].we;
-            console.log(event);
-          console.log(poligono.getPaths);
-          console.log(event.latLngs.we[0].we) */
-			});
-        
-     /*  google.maps.event.addListener(drawTools, "click", function(polygon) {
-				
-        geocercas.slice(polygon)
-        console.log(geocercas)
-       });
- */
-      
-      
-      
+                }
+            });
 
 
-     
+            if (IsAdmin()) {
+                drawTools.setMap(map);
+            }
 
-     
+
+            if (cargargeocercas()) {
+
+
+                let cargageo = cargargeocercas();
+                cargageo.forEach(element => {
+
+                    const contentString = 'contenido basico';
+
+                    const infowindow = new google.maps.InfoWindow({content: contentString});
+
+
+                    // console.log(element);
+
+                    var pats = element.geocerca;
+                    var name = element.nombre;
+                    var domicilio = element.domicilio;
+
+                    let poli = new google.maps.Polygon({
+                        fillColor: "#0008ff",
+                        fillOpacity: 0.5,
+                        strokeWeight: 1,
+                        clickable: true,
+                        editable: false,
+                        zIndex: 1,
+                        draggable: false,
+                        paths: pats
+
+                    });
+
+
+                    geocercas.push({'id': index, 'nombre': name, 'domicilio': domicilio, 'geocerca': pats})
+
+                    index++;
+                    poli.setMap(map);
+                    google.maps.event.addListener(poli, 'mouseover', (polygon) => {
+                        var contentString = "<h1>Lugar: " + name + "</h1><br><h3>" + domicilio + "</h3><br>" + polygon.latLng.toUrlValue(6);
+                        infowindow.setContent(contentString);
+                        infowindow.setPosition(polygon.latLng);
+                        infowindow.open(map);
+
+                    })
+                    google.maps.event.addListener(poli, 'mouseout', function () {
+                        infowindow.close();
+                    });
+
+
+                });
+            }
+
+
+            google.maps.event.addListener(drawTools, "polygoncomplete", (polygon) => { // console.log(polygon.latLngs.we[0].we);
+                var paths = polygon.latLngs.we[0].we;
+                let name: string = window.prompt("Cual es el nombre del lugar") || '';
+                let domicilio: string = window.prompt("Cual es el domicilio del lugar") || '';
+                let lati = paths[0].lat;
+                let longi = paths[0].lng;
+
+
+                let lugar22: Lugar = {
+                    'nombre': name,
+                    'domicilio': domicilio
+                };
+                const nueva = () => {
+                    this.map2service.getLugares().subscribe(data => {
+                        let sitios: sitio[];
+                        sitios = data;
+                        console.log(data);
+
+
+                    })
+                } 
+                
+                nueva();
+
+
+                geocercas.push({'id': index, 'nombre': name, 'domicilio': domicilio, 'geocerca': paths})
+
+                index++;
+
+                // console.log(lati(), longi())
+                addListenersOnPolygon(geocercas);
+
+                guardageocercas(JSON.stringify(geocercas));
+
+
+            });
+
+
+        } 
+        initMap();
+
+
     }
 
-    
-    initMap();
-
-
-  }
-  
-  
-
 }
+
+
 function guardageocercas(guardar: string) {
-  localStorage.setItem('geocercas',guardar);
+    localStorage.setItem('geocercas', guardar);
 }
 
-function cargargeocercas(){
-  
-  if (verificageocercas()!= null) {
-   return verificageocercas()
-  }
-  return
+function cargargeocercas() {
+
+    if (verificageocercas() != null) {
+        return verificageocercas()
+    }
+    return
 }
 
-function verificageocercas(){
-  if(localStorage.getItem('geocercas')) {
-    return JSON.parse(localStorage.getItem('geocercas')!)
-  }
-  return null
+function verificageocercas() {
+    if (localStorage.getItem('geocercas')) {
+        return JSON.parse(localStorage.getItem('geocercas')!)
+    }
+    return null
 }
 
-function IsAdmin(){
-  let usuario: user;
+function IsAdmin() {
+    let usuario: user;
 
-  var token = localStorage.getItem('auth_token');
-  var decode = jwtDecode(token!);
-  var decode2 = JSON.stringify(decode);
-  usuario = JSON.parse(decode2);
- console.log("d",decode)
- console.log("d2",decode2)
- console.log("usuario",usuario)
-  //usuario = JSON.parse(decode);
-//   console.log(usuario.role);
-if(usuario.role == "admin"){
-  return true;
-}else{
-  return false;
-}
-  
+    var token = localStorage.getItem('auth_token');
+    var decode = jwtDecode(token !);
+    var decode2 = JSON.stringify(decode);
+    usuario = JSON.parse(decode2);
+    // console.log("d", decode)
+    // console.log("d2", decode2)
+    // console.log("usuario", usuario)
+    // usuario = JSON.parse(decode);
+    // console.log(usuario.role);
+    if (usuario.role == "admin") {
+        return true;
+    } else {
+        return false;
+    }
+
+
 }
